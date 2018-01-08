8d3b8b4086d12e28515b11bba10e96b54c34bcd9
Fred Wong <fredwong.it@gmail.com>
2018-01-08 11:43:05 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'fred',
-        text: 'Hey, this is fred.'
-    });
 });
 
 socket.on('disconnect', function () {
